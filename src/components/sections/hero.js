ddfa7e1e8dfe4555fcd8b154c9889f7f7c67b5ce
MANISH CHOUHAN <manish.19bcon323@jecrcu.edu.in>
2022-09-19 11:21:05 +0530

    Hero updates

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,28 +59,20 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Manya Verma</h2>;
+  const three = <h3 className="big-heading">I love to Code , Travel and Collaborate.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a MERN Full stack web developer from Jaipur,India developing solutions which decode real
+        life scenarios. Currently I am more focussed towards web solutions to real world problem
+        with automation.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="manyapawanverma@gmail.com" target="_blank" rel="noreferrer">
+      !
     </a>
   );
 
